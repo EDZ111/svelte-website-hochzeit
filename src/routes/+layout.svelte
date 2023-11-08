325d841a9ceb60f8eafb5a24b8ab79d3eb8e74b0
EDZ111 <edoardo.cre@gmail.com>
2023-11-08 07:39:49 +0100

    image size

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,12 +10,12 @@
   <slot />
 </div>
  -->
- <div class="relative">
-  <!-- Background image with custom opacity -->
-  <div class="content-olivebranch bg-cover absolute inset-0 opacity-5"></div>
+ <div class="fixed inset-0 flex items-center justify-center">
+  <!-- Background image (SVG) covering the entire screen -->
+  <div class="content-olivebranch bg-cover bg-center absolute inset-0 opacity-5"></div>
 
-  <!-- Slot element with full opacity -->
-  <div class="relative z-10">
-    <slot/>
+  <!-- Slot element with 75% height -->
+  <div class="relative z-10 h-3/4">
+    <slot></slot>
   </div>
 </div>
